0adfb4354191311d693600b5db477b4f815e1f99
stevechan <chanshunli@gmail.com>
2024-04-18 23:07:41 +0800

    Initial Commit

---
+++ HuluWord/HuluWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuluWordApp.swift
+//  HuluWord
+//
+//  Created by lisp on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct HuluWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
